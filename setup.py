13d76ffe1e065e1b8f58b72b75296dba5062b739
David Muller <dmuller@guidebook.com>
2017-11-29 12:05:46 -0800

    Add foundational files for pip packaging

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+github_repo_url = 'https://github.com/Guidebook/guidebook-api-python'
+
+setup(
+    name='guidebook-api-python',
+    version='0.0.1',
+    author='Guidebook',
+    author_email='web-team@guidebook.com',
+    description='Python client for the Guidebook Open API.',
+    long_description='See {} for installation and usage instructions.'.format(github_repo_url),
+    url=github_repo_url,
+    license='MIT',
+    packages=['guidebook'],
+    install_requires=['requests>=2.0.0'],
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+    ],
+)
